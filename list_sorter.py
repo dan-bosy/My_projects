3dfda960877a58ab31c832b003685a1ce2e977fc
dan-bosy <ai.node.network@gmail.com>
2025-05-06 20:41:18 +0100

    Update list_sorter.py

--- list_sorter.py
+++ list_sorter.py
@@ -1,10 +1,10 @@
 def sort_colors(lst):
-    sorted = []
+    result = []
     max_index = max(lst) + 1
     numbers = [x for x in range(max_index)]
     for i in numbers:
             for j in lst:
                 if j == i:
-                    sorted.append(j)
-    print(sorted)
+                    result.append(j)
+    print(result)
     
