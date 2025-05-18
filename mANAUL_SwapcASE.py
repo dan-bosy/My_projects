f841077600627554195e5b3cc7320f92379e6334
dan-bosy <ai.node.network@gmail.com>
2025-05-18 08:29:54 +0100

    Create mANAUL_SwapcASE.py

---
+++ mANAUL_SwapcASE.py
@@ -0,0 +1,10 @@
+
+def manual_swap_case(s):
+    swapped = ''
+    for char in s:
+        if char.isupper():
+            swapped += char.lower()
+        elif char.lower():
+           swapped += char.upper()
+           
+    return swappe
